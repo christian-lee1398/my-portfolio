d03a775d56ad6cc0c8992d4d005caf208a1dcbc1
Christian Lee <christian.lee1398@gmail.com>
2023-03-12 22:52:01 -0400

    add links/styling

---
+++ src/data.js
@@ -0,0 +1,38 @@
+import YelpCamp from './images/YelpCampHome.png';
+import IRC from './images/rating-comp.png';
+import FAQ from './images/desktop-preview copy.png';
+import AdviceGen from './images/advicegen.png';
+export const projects = [
+    {
+        title: "YelpCamp",
+        subtitle: "MongoDB, Express, JS, Node Stack",
+        description: "This was a large project completed alongside Colt Steele's Web Developer Bootcamp via Udemy",
+        image: YelpCamp,
+        link: "https://pure-forest-68012.herokuapp.com/",
+        github: "https://github.com/christian-lee1398/YelpCamp"
+    },
+    {
+        title: "Interactive Rating Component",
+        subtitle: "HTML, CSS, JS",
+        description: "A rating card allowing a rating out of 5",
+        image: IRC,
+        link: "https://christian-lee1398.github.io/interactive-rating-component/",
+        github: "https://github.com/christian-lee1398/interactive-rating-component"
+    },
+    {
+        title: "FAQ Accordion Page",
+        subtitle: "HTML, CSS, JS",
+        description: "Sample FAQ card component",
+        image: FAQ,
+        link: "https://christian-lee1398.github.io/FAQ-accordion-card/",
+        github: "https://github.com/christian-lee1398/FAQ-accordion-card"
+    },
+    {
+        title: "Advice Generator using Axios",
+        subtitle: "HTML, CSS, JS, Axios",
+        description: "Random advice generator using API",
+        image: AdviceGen,
+        link: "https://christian-lee1398.github.io/advice-generator/",
+        github: "https://github.com/christian-lee1398/advice-generator"
+    },
+]
