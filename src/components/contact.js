77e9d3e77113fd5aaf0e825b3d5fb642ecf2caae
Christian Lee <christian.lee1398@gmail.com>
2023-04-19 19:00:25 -0400

    add form inputs

--- src/components/contact.js
+++ src/components/contact.js
@@ -15,8 +15,8 @@ class Contact extends Component {
                             style={{ filter: "opacity(0.7)" }}
                             src="https://www.google.com/maps/embed/v1/place?q=Farmingdale,+NY,+USA&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
                         />
-                        <div className='map-address-container'>
-                            <div className='map-address-content'>
+                        <div className='map-info-container'>
+                            <div className='map-address'>
                                 <h2>
                                     ADDRESS
                                 </h2>
@@ -24,15 +24,51 @@ class Contact extends Component {
                                     38 Hudson Street<br></br>Farmingdale, NY 11735
                                 </p>
                             </div>
-                            <div className='map-address-info'>
+                            <div className='map-contact-info'>
                                 <h2>EMAIL</h2>
                                 <a>christian.lee1398@gmail.com</a>
                                 <h2>PHONE</h2>
                                 <p>603-732-3669</p>
                             </div>
-
                         </div>
                     </div>
+                    <form
+                        netlify
+                        name='contact'
+                        className='contact-form'>
+                        <h2>Hire Me</h2>
+                        <p>Please send your information over to me if you are interested in either working with or hiring me for your company</p>
+                        <div className='name-entry'>
+                            <label htmlFor='name'>Name</label>
+                            <input
+                                type='text'
+                                id='name'
+                                name='name'
+                            ></input>
+                        </div>
+                        <div className='email-entry'>
+                            <label htmlFor='email'>Email</label>
+                            <input
+                                type='email'
+                                id='email'
+                                name='email'
+                            ></input>
+                        </div>
+                        <div className='message-entry'>
+                            <label htmlFor='message'>Message</label>
+                            <textarea
+                                type='text'
+                                id='message'
+                                name='message'
+                            ></textarea>
+                        </div>
+                        <button
+                            type='submit'
+                            className='submit-button'
+                        >
+                            Submit
+                        </button>
+                    </form>
                 </div>
             </section>
         )
